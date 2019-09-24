fc6013cd2c1eb9c86772768dec7d9d4ec13af17b
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-23 21:36:41 -0500

    Generate sequence of colors

--- script.js
+++ script.js
@@ -8,14 +8,26 @@ const green = document.getElementById('verde')
 class Game {
     constructor(){
         this.initialize()
+        this.generateSequence()
     }
 
     initialize(){
         begin_button.classList.add('hide')
+        this.level = 1
+        this.colors = {
+            sky_blue,
+            violet,
+            orange,
+            green
+        }
+    }
+
+    generateSequence(){
+        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 }
 
 
 function beginGame(){    
-   var game = new Game()
+   window.game = new Game()
 }
